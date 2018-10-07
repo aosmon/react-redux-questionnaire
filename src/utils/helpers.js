27d28e539336ee939e2805a6ff27ffeb4060b16f
Aizhan Osmonova <osmonova86@gmail.com>
2018-10-07 10:58:34 -0400

    feat: Move formatQuestion to helpers.js

---
+++ src/utils/helpers.js
@@ -0,0 +1,15 @@
+function formatQuestion ({ optionOneText, optionTwoText, author }) {
+  return {
+    id: generateUID(),
+    timestamp: Date.now(),
+    author,
+    optionOne: {
+      votes: [],
+      text: optionOneText,
+    },
+    optionTwo: {
+      votes: [],
+      text: optionTwoText,
+    }
+  }
+}
