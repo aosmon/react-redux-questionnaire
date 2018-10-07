ed5b549d5baec379fc4cd800a1180966f35d488b
Aizhan Osmonova <osmonova86@gmail.com>
2018-10-06 21:30:37 -0400

    feat: Connect Dashboard to the store and access question ids

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 class Dashboard extends Component{
 	render() {
+		console.log(this.props);
 		return (
 			<div>
+				<h3>Questions</h3>
+				<ul>
+					{this.props.questionIds.map((id)=>(
+						<li key={id}>
+							<div>{id}</div>
+						</li>
+					))}
+				</ul>
 			</div>
 		)
 	}
 }
 
-export default Dashboard
+function mapStateToProps({questions}){
+	return{
+		questionIds: Object.keys(questions)
+		.sort((a,b) => questions[b].timestamp - questions[a].timestamp)		
+	}
+}
+
+export default connect(mapStateToProps)(Dashboard)
