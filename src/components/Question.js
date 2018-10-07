c82be09356505125bf4f4622ac0964ad4208f5a1
Aizhan Osmonova <osmonova86@gmail.com>
2018-10-07 10:39:19 -0400

    feat: Connect Question component to the store and map to props

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { formatQuestion } from '../utils/helpers'
 
 class Question extends Component{
 	render() {
 		return (
-			<div>
+			<div className='question'>
 			</div>
 		)
 	}
 }
 
-export default Question
+mapStateToProps({authedUser, questions, users}{id}){
+	const question = questions[id]
+
+	return {
+		authedUser,
+		tweet: formatQuestion(question, users[question.author], authedUser)
+	}
+}
+
+export default connect(mapStateToProps)(Question)
