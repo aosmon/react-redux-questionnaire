04222a812254afbd89793b1b9d9cf8d5ffa93ca1
Aizhan Osmonova <osmonova86@gmail.com>
2018-10-07 15:12:43 -0400

    feat: Update formatQuestion to return answer selected by authedUser

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2,6 +2,12 @@ export function formatQuestion ( question, author, authedUser ) {
 
   const { id, optionOne, optionTwo, timestamp } = question
   const { name, avatarURL } = author
+  let answer = null;
+  if(optionOne.votes.includes(authedUser)){
+    answer = optionOne
+  }else if(optionTwo.votes.includes(authedUser)){
+    answer = optionTwo
+  }
 
   return {
     author: name,
@@ -9,8 +15,14 @@ export function formatQuestion ( question, author, authedUser ) {
     timestamp,
     avatar: avatarURL,
     optionOne: {
+      votes: optionOne.votes,
       text: optionOne.text,
     },
-    hasAnswered: optionOne.votes.includes(authedUser) || optionTwo.votes.includes(authedUser)
+    optionTwo: {
+      votes: optionTwo.votes,
+      text: optionTwo.text,
+    },
+    hasAnswered: optionOne.votes.includes(authedUser) || optionTwo.votes.includes(authedUser),
+    answer: answer
   }
 }
