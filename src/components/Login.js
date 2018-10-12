bf9022c9491e78c86008f1c4792122e673ec6d94
Aizhan Osmonova <osmonova86@gmail.com>
2018-10-12 11:04:33 -0400

    feat: Style Login component and connect to the store

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,48 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import ReduxLogo from '../images/ReduxLogo.jpg'
 
 class Login extends Component{
+	
+	handleLogout= () => {
+		
+	}
+
 	render() {
+		console.log(this.props);
+		const {users} = this.props
+
 		return (
-			<div>
+			<div className='login container'>
+				<div className='header'>
+					<h1>Welcome to the Would You Rather App!</h1>
+					<p>Please sign in to continue</p>
+				</div>
+				<div className='content'>
+					<img src={ReduxLogo} />
+					<h2>Sign in</h2>
+					<select name="users" placeholder='Select User'>
+						<option value=''>Select User</option>
+						{this.props.userIds.map((id)=>(
+							 <option value={id}>
+							 	{users[id].name}
+							 </option>
+						))}
+					</select>
+					<button className='login' onClick={(e)=>this.handleLogout(e)}>Sign In</button>
+				</div>
 			</div>
 		)
 	}
 }
 
-export default Login
+function mapStateToProps({authedUser, users}){
+
+  return {
+  	authedUser,
+    users,
+    userIds: Object.keys(users)
+  }
+}
+
+export default connect(mapStateToProps)(Login)
