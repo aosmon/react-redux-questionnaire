2449f4af0ace05d6fe9b5913294ed578b5ab1317
Aizhan Osmonova <osmonova86@gmail.com>
2018-10-14 00:03:40 -0400

    feat: Add tabs for Dashboard to toggle answered/unaswered questions

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,25 +3,59 @@ import { connect } from 'react-redux'
 import Question from './Question'
 
 class Dashboard extends Component{
+
+	state = {
+		activeTab: 'unanswered'
+	}
+
+	toggleTab = (tab) => {
+		this.setState(()=>({
+			activeTab: tab
+		}))
+	}
+
 	render() {
+
+		const {activeTab} = this.state
+
 		return (
 			<div className='dashboard container'>
-				<h3>Answered Questions</h3>
-				<ul className='question-list'>
-					{this.props.answeredIds.map((id)=>(
-						<li key={id}>
-							<Question id={id} />
-						</li>
-					))}
-				</ul>
-				<h3>Unanswered Questions</h3>
-				<ul>
-					{this.props.unansweredIds.map((id)=>(
-						<li key={id}>
-							<Question id={id} />
-						</li>
-					))}
-				</ul>
+				<div className='tabs'>
+					<button
+						className={activeTab==='answered'? 'tab active' : 'tab'}
+						onClick={()=>{this.toggleTab('answered')}}
+						disabled={activeTab==='answered'}
+					>
+						<h3>Answered Questions</h3>
+					</button>
+					<button
+						className={activeTab==='unanswered'? 'tab active' : 'tab'}
+						onClick={()=>{this.toggleTab('unanswered')}}
+						disabled={activeTab==='unanswered'}
+					>
+						<h3>Unanswered Questions</h3>
+					</button>				
+				</div>
+				<div className='content'>					
+					{activeTab==='answered' && (
+						<ul className='question-list'>
+							{this.props.answeredIds.map((id)=>(
+								<li key={id}>
+									<Question id={id} />
+								</li>
+							))}
+						</ul>				
+					)}
+					{activeTab==='unanswered' && (
+						<ul>
+							{this.props.unansweredIds.map((id)=>(
+								<li key={id}>
+									<Question id={id} />
+								</li>
+							))}
+						</ul>
+					)}
+				</div>
 			</div>
 		)
 	}
